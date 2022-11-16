2898419d9b50170c4f9749ecd026d95951f2dcce
Paul Haines <Paul.Haines@PawQualityProjects.co.uk>
2022-11-16 07:44:25 -0500

    tidying up

--- Prototype/isobmff/meta.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from .box import FullBox
-from .box import Quantity
-
-
-class MetaBox(FullBox):
-    box_type = 'meta'
-    is_mandatory = False
-    quntity = Quantity.ZERO_OR_ONE
